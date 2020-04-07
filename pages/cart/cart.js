a96d053bc0ffc91af9b0f89af990cfc9e0c77007
unknown <unknown@example.com>
2020-04-07 12:31:13 +0800

    初始化cart

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -1,4 +1,6 @@
 // pages/cart/cart.js
+// import Cart from './cart-model.js';
+// var cart = new Cart();
 Page({
 
   /**
@@ -15,12 +17,6 @@ Page({
 
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
 
   /**
    * 生命周期函数--监听页面显示
@@ -29,38 +25,4 @@ Page({
 
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
