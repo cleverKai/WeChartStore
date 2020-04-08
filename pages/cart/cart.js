259bd6907beec5120c5456f88ac2b49985a9d457
cleverKai <951040436@qq.com>
2020-04-08 14:49:57 +0800

    计算购物车当前选择商品的总数，总价格，和种类数

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -1,6 +1,6 @@
 // pages/cart/cart.js
-// import Cart from './cart-model.js';
-// var cart = new Cart();
+import {Cart} from './cart-model.js';
+var cart = new Cart();
 Page({
 
   /**
@@ -22,7 +22,39 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    var cartData = cart.getCartDataFromLocal();
+    // var countsInfo = cart.getCartTotalCounts(true);
+    var calcData = this._calcTotalAccountAndCounts(cartData);
+    this.setData({
+      //选中商品的数量
+      selectedCounts:calcData.selectedCounts,
+      selectedTypeCounts:calcData.selectedTypeCounts,
+      account:calcData.account,
+      //购物车里面所有商品的数量
+      cartData:cartData
+    });
   },
-
+  _calcTotalAccountAndCounts:function(data){
+      var len = data.length;
+      // 计算出来的总价格，但是要排除未选中的商品
+      var account = 0;
+      // 购买商品的总个数，但是要排斥未选中的商品
+      var selectedCounts = 0;
+      // 购买商品种类的数目
+      var selectedTypeCounts = 0;
+      let multiple = 100;
+      //计算总价格
+      for(let i=0;i<len;i++){
+        if(data[i].selectStatus){
+          account += data[i].counts*multiple*Number(data[i].price)*multiple;
+          selectedCounts += data[i].counts;
+          selectedTypeCounts++;
+        }
+      }
+      return {
+        selectedCounts:selectedCounts,
+        selectedTypeCounts:selectedTypeCounts,
+        account:account/(multiple*multiple)
+      }
+  }
 })
